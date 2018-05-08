46e718bdce21e0cdfb8631e868fca8fc45a450c9
Jozef Piecyk <jozollo@gmail.com>
2018-05-08 21:56:07 +0200

    change cell to stateless component

--- src/board/cell.jsx
+++ src/board/cell.jsx
@@ -1,37 +1,19 @@
 // @flow
 
-import React, { Component } from 'react';
+import React from 'react';
 
-type CellProps = {
-  onCellClicked: Function,
-  cellState: CellState
+const Cell = (props: { onCellClicked: Function, cellState: CellState }) => {
+  const cellClicked = () => {
+    props.onCellClicked(props.cellState.coordinates);
+  };
+  return (
+    <button className="board-cell" onClick={cellClicked} onKeyPress={cellClicked}>
+      <p>row - {props.cellState.coordinates.x}</p>
+      <p>col - {props.cellState.coordinates.y}</p>
+      <p>unit - {props.cellState.unitType}</p>
+      <p>current player = {props.cellState.color}</p>
+    </button>
+  );
 };
-export class Cell extends Component<CellProps, null> {
-  cellClicked = () => {
-    this.props.onCellClicked(this.props.cellState.coordinates);
-  };
-
-  props: {
-    cellState: {
-      color: string,
-      coordinates: Object & { x: number, y: number },
-      unitType: string,
-      polarity: string,
-      selected: boolean
-    },
-    onCellClicked: Function
-  };
-
-  render() {
-    return (
-      <button className="board-cell" onClick={this.cellClicked} onKeyPress={this.cellClicked}>
-        <p>row - {this.props.cellState.coordinates.x}</p>
-        <p>col - {this.props.cellState.coordinates.y}</p>
-        <p>unit - {this.props.cellState.unitType}</p>
-        <p>current player = {this.props.cellState.color}</p>
-      </button>
-    );
-  }
-}
 
 export default Cell;
