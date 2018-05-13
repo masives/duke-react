1ab069c45979a7ad399af6efe07f5d78bc41b0eb
Jozef Piecyk <jozollo@gmail.com>
2018-05-13 20:48:48 +0200

    add typing

--- flow-typed/types.js
+++ flow-typed/types.js
@@ -16,3 +16,28 @@ export type CellState = {
 
 // prettier-ignore
 export type BoardCells = Array < Array < CellState >> ;
+
+declare
+var module: {
+  hot: {
+    accept(path: string, callback: () => void): void
+  }
+};
+
+export type GameState = {
+  board: BoardCells,
+  gameStage: string,
+  targetedCell: CellState | null,
+  currentPlayer: string
+};
+
+export type initialSetupState = {
+  white: Object & {
+    dukeDrawn: boolean,
+    knightsDrawn: number
+  },
+  black: Object & {
+    dukeDrawn: boolean,
+    knightsDrawn: number
+  },
+}
