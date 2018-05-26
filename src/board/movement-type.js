2d30854551659a498570698382c0aabe925d0a00
Jozef Piecyk <jozollo@gmail.com>
2018-05-26 22:00:07 +0200

    add horizontal duke movement

--- src/board/movement-type.js
+++ src/board/movement-type.js
@@ -22,8 +22,53 @@ export const getUnitMovement = (
       col: currentCoordinates.col + coordinates.col
     }));
   }
+  if (result.slide) {
+    if (result.slide === 'horizontal') {
+      result = [];
+
+      let checkedLeftIndex = 1;
+      const checkLeft = () => {
+        if (
+          currentCoordinates.col - checkedLeftIndex >= 0 &&
+          board[currentCoordinates.row][currentCoordinates.col - checkedLeftIndex].color !== currentPlayer
+        ) {
+          console.log('czy warunek?', currentCoordinates.col - checkedLeftIndex);
+          result.push({
+            row: currentCoordinates.row,
+            col: currentCoordinates.col - checkedLeftIndex
+          });
+          checkedLeftIndex += 1;
+          checkLeft();
+        }
+      };
+      let checkedRightIndex = 1;
+      const checkRight = () => {
+        if (
+          currentCoordinates.col + checkedRightIndex < 6 && // tu ma być szerokośc z configa
+          board[currentCoordinates.row][currentCoordinates.col + checkedRightIndex].color !== currentPlayer
+        ) {
+          console.log('czy warunek?', currentCoordinates.col + checkedRightIndex);
+          result.push({
+            row: currentCoordinates.row,
+            col: currentCoordinates.col + checkedRightIndex
+          });
+          checkedRightIndex += 1;
+          checkRight();
+        }
+      };
+      checkLeft();
+      checkRight();
+      console.log('horizontalMove', result);
+
+      // jeśli nie ma tam przyjazne jednostki to dodaj do arayki i spróbuje dalej
+      // jeśli jest to zakończ i zacznij z prawej
+
+      // to samo tylko że potem zacznij
+    }
+  }
 
   // delete out of bounds
+  console.log('result before filter', result);
   result = result
     .filter(coordinates => isInBounds(coordinates))
     // check for friendly collision
