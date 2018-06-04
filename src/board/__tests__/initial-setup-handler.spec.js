2c3058e2f759805c95f550d74d6b3c50934230f3
Jozef Piecyk <jozollo@gmail.com>
2018-06-04 21:34:05 +0200

    add duke setup tests

--- src/board/__tests__/initial-setup-handler.spec.js
+++ src/board/__tests__/initial-setup-handler.spec.js
@@ -1,7 +1,9 @@
 // @flow
 
 import InitialSetupHandler from '../initial-setup-handler';
-import emptyBoard from './test-helpers/board-empty.json';
+import emptyBoard from './test-helpers/board/board-empty.json';
+import emptyDukeCoordinates from './test-helpers/duke-coordinates/empty.json';
+import PLAYER_COLOR from '../enums/player-color';
 
 test('Should initialize correctly', () => {
   const initialSetupHandler = new InitialSetupHandler();
@@ -32,7 +34,102 @@ test('Should return error message when trying to set in wrong row', () => {
     startingSide: true,
     state: ''
   };
-  const result = initialSetupHandler.handleInitialSetup(invalidTargetCell, 'white', emptyBoard); // one argument is missing here
+  const result = initialSetupHandler.handleInitialSetup(
+    invalidTargetCell,
+    PLAYER_COLOR.WHITE,
+    emptyBoard,
+    emptyDukeCoordinates
+  ); // one argument is missing here
 
   expect(result.message).toBe('wrong row - please choose the top row');
+});
+
+test("Should return board with duke and update it's coordinates[WHITE]", () => {
+  // given
+  const initialSetupHandler = new InitialSetupHandler();
+  const currentPlayer = PLAYER_COLOR.WHITE;
+  const validWhiteDukeTargetCell: CellState = {
+    coordinates: {
+      row: 0,
+      col: 2
+    },
+    color: '',
+    unitType: '',
+    startingSide: true,
+    state: ''
+  };
+
+  // when
+  const result = initialSetupHandler.handleInitialSetup(
+    validWhiteDukeTargetCell,
+    currentPlayer,
+    emptyBoard,
+    emptyDukeCoordinates
+  );
+
+  // then
+  expect(result.board[validWhiteDukeTargetCell.coordinates.row][validWhiteDukeTargetCell.coordinates.col]).toEqual(
+    expect.objectContaining({
+      color: currentPlayer,
+      unitType: 'duke'
+    })
+  );
+  expect(result.dukeCoordinates).toEqual(
+    expect.objectContaining({
+      white: expect.objectContaining({
+        row: validWhiteDukeTargetCell.coordinates.row,
+        col: validWhiteDukeTargetCell.coordinates.col
+      })
+    })
+  );
+});
+
+test("Should return board with duke and update it's coordinates[BLACK]", () => {
+  // given
+  const initialSetupHandler = new InitialSetupHandler();
+  initialSetupHandler.initialSetupState = {
+    white: {
+      dukeDrawn: true,
+      footmanDrawn: 2
+    },
+    black: {
+      dukeDrawn: false,
+      footmanDrawn: 0
+    }
+  };
+  const currentPlayer = PLAYER_COLOR.BLACK;
+  const validBlackDukeTargetCell: CellState = {
+    coordinates: {
+      row: 5,
+      col: 2
+    },
+    color: '',
+    unitType: '',
+    startingSide: true,
+    state: ''
+  };
+
+  // when
+  const result = initialSetupHandler.handleInitialSetup(
+    validBlackDukeTargetCell,
+    currentPlayer,
+    emptyBoard,
+    emptyDukeCoordinates
+  );
+
+  // then
+  expect(result.board[validBlackDukeTargetCell.coordinates.row][validBlackDukeTargetCell.coordinates.col]).toEqual(
+    expect.objectContaining({
+      color: currentPlayer,
+      unitType: 'duke'
+    })
+  );
+  expect(result.dukeCoordinates).toEqual(
+    expect.objectContaining({
+      black: expect.objectContaining({
+        row: validBlackDukeTargetCell.coordinates.row,
+        col: validBlackDukeTargetCell.coordinates.col
+      })
+    })
+  );
 });
